73040012d670425ed8fe2669fe1db5cf23db9eb8
Luka Salonen <luka@exove.fi>
2025-03-25 15:09:13 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="tw:text-3xl tw:text-center tw:my-8">
+        <h1>Piwik PRO Dashboard</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
