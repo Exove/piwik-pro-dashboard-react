66d323995ac9c5c47b361e98c910a734de37cef6
Luka Salonen <luka@exove.fi>
2025-04-09 13:01:52 +0300

    CARE-5711 stylem improvements to line charts and pie charts

--- src/components/LineChartBox.tsx
+++ src/components/LineChartBox.tsx
@@ -26,12 +26,24 @@ export default function LineChartBox({ title, data }: LineChartBoxProps) {
       <ResponsiveContainer width="100%" height={300}>
         <LineChart data={data}>
           <CartesianGrid stroke="#ccc" />
-          <XAxis dataKey="time" />
+          <XAxis
+            dataKey="time"
+            tickFormatter={(timeStr) => {
+              const date = new Date(timeStr);
+              return date.toLocaleDateString(undefined, {
+                month: 'short',
+                day: 'numeric',
+              });
+            }}
+            tick={{ fontSize: 16 }}
+            interval={1}
+          />
           <YAxis
             domain={[
               (dataMin: number) => Math.floor(dataMin * 0.9),
               (dataMax: number) => Math.ceil(dataMax * 1.1),
             ]}
+            tick={{ fontSize: 16 }}
           />
           <Tooltip />
           <Line type="monotone" dataKey="value" stroke="#8884d8" />
